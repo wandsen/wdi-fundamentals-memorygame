8f3ce910471fddaf249371e4194e6c6d2353333f
Wan D Sen <dsen@Wans-MacBook-Pro.local>
2018-08-26 00:10:57 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("user flipped" + " " + cardOne);
+console.log("user flipped" + " " + cardTwo);
