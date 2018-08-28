4e4f638051710c5562cbc65a36f9a7ad2cfcc9a1
Wan D Sen <dsen@Wans-MacBook-Pro.local>
2018-08-28 21:52:18 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("user flipped" + " " + cardOne);
-console.log("user flipped" + " " + cardTwo);
+
+cards = ["queen", "queen", "king", "king"];
+
+cardsInPlay = [];
+
+cardOne = cards[0];
+
+
+cardsInPlay.push(cardOne);
+console.log("User flipped queen")
+
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped King")
+
+if (cardsInPlay.length >= 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("you found a match");
+	}
+
+	else {
+		alert("Sorry, try again.");
+	}
+}
