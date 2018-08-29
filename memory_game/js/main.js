5c654320b1618e8f7429264054deee72eb9058b3
Wan D Sen <dsen@Wans-MacBook-Pro.local>
2018-08-30 03:38:07 +0800

    created arrays and objects

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,25 @@
 console.log("Up and running!");
 
-cards = ["queen", "queen", "king", "king"];
+var cards = [
 
-cardsInPlay = [];
+	{rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts/png"},
+
+	{rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds/png"},
+
+	{rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts/png"},
+
+	{rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds/png"}
+];
+
+var cardsInPlay = [];
 
 
 var checkForMatch = function(){
@@ -16,10 +33,10 @@ var checkForMatch = function(){
 
 var flipCard = function(cardId)
 {
-	console.log("User flipped" + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
-
+	console.log("Use flipped " + cards[cardId].rank + "of " + cards[cardId].suit + cards[cardId].cardImage);
 
 }
 
